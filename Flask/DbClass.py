95b9fe487b0f11a6219eaff36cdf4d3558ce7540
Jamie Fong <jamie.fong@student.howest.be>
2017-06-11 10:45:15 +0200

    Alle webpagina's, voornaam ophalen, gebruiker ophalen,...

--- Flask/DbClass.py
+++ Flask/DbClass.py
@@ -22,7 +22,7 @@ class DbClass:
 
         self.__cursor.execute(sqlQuery)
         result = self.__cursor.fetchall()
-        self.__cursor.close()
+        # self.__cursor.close()
         return result
 
     def getDataFromDatabaseMetVoorwaarde(self, voorwaarde):
@@ -33,7 +33,7 @@ class DbClass:
 
         self.__cursor.execute(sqlCommand)
         result = self.__cursor.fetchall()
-        self.__cursor.close()
+        # self.__cursor.close()
         return result
 
     def setDataToDatabase(self, value1):
@@ -44,10 +44,60 @@ class DbClass:
 
         self.__cursor.execute(sqlCommand)
         self.__connection.commit()
-        self.__cursor.close()
+        # self.__cursor.close()
 
     def tijdDoorsturenNaarDb(self):
         query = "INSERT INTO `project_db`.`meting`(`Tijdstip gedrukt`,`WaterbekerID`,`gebruiker_ID`) VALUES (NOW(),1,1);"
         self.__cursor.execute(query)
         self.__connection.commit()
         # self.__cursor.close()
+
+    def ophalenTijd(self):
+        query = "SELECT * FROM meting"
+        self.__cursor.execute(query)
+        result = self.__cursor.fetchall()
+        # self.__cursor.close()
+        return result
+
+    def gebruikerAanmaken(self,naam,voornaam,geslacht,gewicht,grootte,activiteit,gebruikersnaam,wachtwoord):
+        query = "INSERT INTO project_db.gebruiker(Naam,Voornaam,Geslacht,Gewicht,Grootte,Activiteit)" \
+                " VALUES('"+naam+"','" +voornaam+ "','" +geslacht+ "','" + gewicht + "','" + grootte + "','" + activiteit + "')" \
+                "; INSERT INTO project_db.inloggegevens(Gebruikersnaam,Wachtwoord) VALUES ('"+gebruikersnaam+"','" + wachtwoord + "') "
+        self.__cursor.execute(query)
+        # data committen!!
+        self.__connection.commit()
+
+    def ophalenVoornaam(self):
+        query = "SELECT Voornaam FROM project_db.gebruiker"
+        self.__cursor.execute(query)
+        result = self.__cursor.fetchone()
+        # self.__cursor.close()
+        return result
+
+    def ophalenGebruiker(self):
+        query = "SELECT Voornaam,Naam,Geslacht,Gewicht,Grootte,Activiteit FROM project_db.gebruiker"
+        self.__cursor.execute(query)
+        result = self.__cursor.fetchall()
+        # self.__cursor.close()
+        return result
+
+
+    def gedronkenPerDag(self, ID):
+        query = "select date(`Tijdstip gedrukt`), count(id) from meting WHERE gebruiker_ID = " + str(ID) + " GROUP BY DATE(`Tijdstip gedrukt`)"
+        self.__cursor.execute(query)
+        result = self.__cursor.fetchone()
+        # self.__cursor.close()
+        return result
+
+
+    def newColor(self, pCode, pName):
+        try:
+            query = "INSERT INTO db_weerstation.kleur " \
+                    "(kleurCode, Naam) " \
+                    "VALUES (%s , %s)" % (pCode, pName)
+            result = self.__cursor.execute(query)
+            self.__connection.commit()
+            return result
+        except:
+            print("Error, kleur is niet toegevoegd.")
+            return None
--- Flask/Project1.py
+++ Flask/Project1.py
@@ -1,26 +1,74 @@
 from flask import Flask
 from flask import render_template
+from DbClass import DbClass
 import os
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def homepage():
+def index():
     return render_template('index.html')
 
-@app.route('/gegevens')
-def gegevens():
-    return render_template('gegevens.html')
+@app.route('/inloggen')
+def inloggen():
+    return render_template('inloggen.html')
 
-@app.route('/profiel')
+@app.route('/registreren', methods=['post','get'])
+def registreren():
+
+    from flask import request
+
+    name    = request.form['naam']
+    firstname   = request.form['voornaam']
+    gender = request.form['geslacht']
+    weight = request.form['gewicht']
+    height = request.form['grootte']
+    activity = request.form['activiteit']
+    username = request.form['gebruikersnaam']
+    password = request.form['wachtwoord']
+
+    # maak database object aan
+    DbLayer = DbClass()
+    DbLayer.gebruikerAanmaken(name,firstname,gender,weight,height,activity,username,password)
+
+    return render_template('registreren.html')
+
+
+
+@app.route('/homepagina')
+def homepagina():
+    DB_layer = DbClass()
+    voornaam = DB_layer.ophalenVoornaam()
+    # voornaam = firstname.rstrip('[','(','\'',',',')',']')
+    return render_template('homepagina.html',voornaam=voornaam[0])
+
+@app.route('/overzicht')
+def gegevens():
+    DB_layer = DbClass()
+    gedronkenPerDag = DB_layer.gedronkenPerDag(1)
+    return render_template('gegevens.html', gedronkenPerDag=gedronkenPerDag)
+
+@app.route('/drinklogboek')
 def profiel():
-    return render_template('profiel.html')
+    DB_layer = DbClass()
+    lijst_metingen = DB_layer.ophalenTijd()
+    # gebruiker = DB_layer.ophalenGebruiker()
+    gebruiker = DB_layer.ophalenGebruiker()
+
+    return render_template('profiel.html', metingen=lijst_metingen, gebruiker=gebruiker[0])
 
 @app.route('/instellingen')
 def instellingen():
     return render_template('instellingen.html')
+
+@app.route('/instellingen/wachtwoordwijzigen')
+def wachtwoordwijzigen():
+    return render_template ('wachtwoordwijzigen.html')
+
+@app.route('/404')
+def pageNotFound():
+    return render_template('error/404.html')
 
 
 if __name__ == '__main__':
